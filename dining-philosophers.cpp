36c8521ae505d36f7a5582ce1111f8c1d2a9398e
zuzuz <duda.zuzanna7@gmail.com>
2025-03-28 20:25:16 +0100

    get philosophers' number and create threads

--- dining-philosophers.cpp
+++ dining-philosophers.cpp
@@ -1,5 +1,30 @@
 #include <iostream>
-int main()
-{
-    std::cout << "Dining Philosophers Problem lets gooo" << std::endl;
+#include <thread>
+#include <chrono>
+#include <vector>
+
+// vector for storing philosophers threads
+std::vector<std::thread> philosophersThreads;
+
+// func that will be run by each thread
+void runPhilosLife(const int philosophersId){
+    std::cout << "Philosopher " << philosophersId << " started thinking..." << std::endl;
+}
+
+int main(){
+    int numberOfPhilosophers;
+    std::cout << "Dining Philosophers Problem lets gooo" << std::endl << std::endl;
+    std::cout << "How many philosophers are at the table?" << std::endl;
+    std::cin >> numberOfPhilosophers;
+
+    // create threads
+    for (int i = 0; i < numberOfPhilosophers; i++){
+        philosophersThreads.emplace_back(runPhilosLife, i);
+    }
+    
+    // wait for them to end
+    for(auto& philosopher : philosophersThreads){
+        philosopher.join();
+    }
+    
 }
