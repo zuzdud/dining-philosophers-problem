b5f52369d84839af711d0050fb465502b0407f44
zuzuz <duda.zuzanna7@gmail.com>
2025-03-28 21:57:33 +0100

    thinking in separate func, and eating func base implemented

--- dining-philosophers.cpp
+++ dining-philosophers.cpp
@@ -14,23 +14,46 @@ std::mt19937 rng{std::random_device{}()};
 std::uniform_int_distribution<int> thinkingTime{500, 2000};
 std::uniform_int_distribution<int> eatingTime{300, 1000};
 
+
+void eat(const int philosopherId)
+{
+    int currentEatingTime = eatingTime(rng);
+    {
+        std::unique_lock<std::mutex> lock(consolePrintingMutex);
+        std::cout << "Philosopher " << philosopherId << " will be eating for " << currentEatingTime << "ms" << std::endl;
+    }
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(currentEatingTime));
+
+    {
+        std::unique_lock<std::mutex> lock(consolePrintingMutex);
+        std::cout << "Philosopher " << philosopherId << " stopped eating..." << std::endl;
+    }
+}
+
+void think(const int philosopherId)
+{
+    int currentThinkingTime = thinkingTime(rng);
+    {
+        std::unique_lock<std::mutex> lock(consolePrintingMutex);
+        std::cout << "Philosopher " << philosopherId << " will be thinking for " << currentThinkingTime << "ms" << std::endl;
+    }
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(currentThinkingTime));
+
+    {
+        std::unique_lock<std::mutex> lock(consolePrintingMutex);
+        std::cout << "Philosopher " << philosopherId << " stopped thinking..." << std::endl;
+    }
+}
+
 // func that will be run by each thread
-void runPhilosLife(const int philosophersId)
+void runPhilosLife(const int philosopherId)
 {
     while (true)
     {
-        int currentThinkingTime = thinkingTime(rng);
-        {
-            std::unique_lock<std::mutex> lock(consolePrintingMutex);
-            std::cout << "Philosopher " << philosophersId << " will be thinking for " << currentThinkingTime << "ms" << std::endl;
-        }
-        
-        std::this_thread::sleep_for(std::chrono::milliseconds(currentThinkingTime));
-
-        {
-            std::unique_lock<std::mutex> lock(consolePrintingMutex);
-            std::cout << "Philosopher " << philosophersId << " stopped thinking..." << std::endl;
-        }
+        think(philosopherId);
+        eat(philosopherId);
     }
 }
 
