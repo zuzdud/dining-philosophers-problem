e2d6a84ba08a789acf5add3aa93422b0723cecc3
zuzuz <duda.zuzanna7@gmail.com>
2025-03-29 19:38:41 +0100

    added comments and formatting

--- dining-philosophers.cpp
+++ dining-philosophers.cpp
@@ -5,68 +5,61 @@
 #include <mutex>
 #include <random>
 
-int numberOfPhilosophers;
+int numberOfPhilosophers = 0;
 std::vector<std::thread> philosophersThreads; // vector for storing philosophers threads
-std::vector<std::mutex> chopsticksMutexes;
-std::mutex consolePrintingMutex;
+std::vector<std::mutex> chopsticksMutexes;    // each chopstick is a mutex
+std::mutex coutMutex;                         // mutex controlling access to console
 
+// generating random thinking and eating times in milliseconds
 std::mt19937 rng{std::random_device{}()};
-std::uniform_int_distribution<int> thinkingTime{500, 2000};
-std::uniform_int_distribution<int> eatingTime{300, 1000};
-
+std::uniform_int_distribution<int> actionTime{2000, 4000};
 
 void eat(const int philosopherId, const int leftChopstick, const int rightChopstick)
 {
-    int currentEatingTime = eatingTime(rng);
+    {
+        std::unique_lock<std::mutex> lock(coutMutex);
+        std::cout << "Philosopher " << philosopherId << " tries to pick up chopsticks " << std::endl;
+    }
 
+    // locking mutexes = picking up chopsticks one by one
     chopsticksMutexes[leftChopstick].lock();
-    std::this_thread::sleep_for(std::chrono::milliseconds(10));
     chopsticksMutexes[rightChopstick].lock();
 
     {
-        std::unique_lock<std::mutex> lock(consolePrintingMutex);
-        std::cout << "Philosopher " << philosopherId << " will be eating for " << currentEatingTime << "ms" << std::endl;
+        std::unique_lock<std::mutex> lock(coutMutex);
+        std::cout << "Philosopher " << philosopherId << " is eating" << std::endl;
     }
 
-    std::this_thread::sleep_for(std::chrono::milliseconds(currentEatingTime));
+    // mutexes are locked for the amount of time that philosopher is eating
+    std::this_thread::sleep_for(std::chrono::milliseconds(actionTime(rng)));
 
-    {
-        std::unique_lock<std::mutex> lock(consolePrintingMutex);
-        std::cout << "Philosopher " << philosopherId << " stopped eating..." << std::endl;
-    }
+    // unlocking mutexes
     chopsticksMutexes[leftChopstick].unlock();
     chopsticksMutexes[rightChopstick].unlock();
 }
 
 void think(const int philosopherId)
 {
-    int currentThinkingTime = thinkingTime(rng);
     {
-        std::unique_lock<std::mutex> lock(consolePrintingMutex);
-        std::cout << "Philosopher " << philosopherId << " will be thinking for " << currentThinkingTime << "ms" << std::endl;
+        std::unique_lock<std::mutex> lock(coutMutex);
+        std::cout << "Philosopher " << philosopherId << " is thinking" << std::endl;
     }
 
-    std::this_thread::sleep_for(std::chrono::milliseconds(currentThinkingTime));
-
-    {
-        std::unique_lock<std::mutex> lock(consolePrintingMutex);
-        std::cout << "Philosopher " << philosopherId << " stopped thinking..." << std::endl;
-    }
+    std::this_thread::sleep_for(std::chrono::milliseconds(actionTime(rng)));
 }
 
 // func that will be run by each thread
-void runPhilosLife(const int philosopherId)
+void philosopherRoutine(const int philosopherId)
 {
-    int leftChopstick, rightChopstick;
+    
+    int leftChopstick = philosopherId;
+    int rightChopstick = (philosopherId + 1) % numberOfPhilosophers;
 
-    if(philosopherId%2==0){
-        leftChopstick = philosopherId;
-        rightChopstick = (philosopherId + 1) % numberOfPhilosophers;
-    } else{
-        leftChopstick = (philosopherId + 1) % numberOfPhilosophers;
-        rightChopstick = philosopherId;
-    }
+    // enforcing different chopstick acquisition orders
+    if (philosopherId % 2 == 1)
+        std::swap(leftChopstick, rightChopstick);
 
+    // main loop of each philosopher
     while (true)
     {
         think(philosopherId);
@@ -78,21 +71,23 @@ int main()
 {
     std::cout << "Dining Philosophers Problem lets gooo" << std::endl
               << std::endl;
+
+    // ensure that there are at least two philosophers
     while (numberOfPhilosophers < 2)
     {
         std::cout << "How many philosophers are at the table? (at least two)" << std::endl;
         std::cin >> numberOfPhilosophers;
     }
 
+    // create mutexes
+    chopsticksMutexes = std::vector<std::mutex>(numberOfPhilosophers);
+
     // create threads
     for (int i = 0; i < numberOfPhilosophers; i++)
     {
-        philosophersThreads.emplace_back(runPhilosLife, i);
+        philosophersThreads.emplace_back(philosopherRoutine, i);
     }
 
-    chopsticksMutexes = std::vector<std::mutex>(numberOfPhilosophers);
-
-    // wait for them to end
     for (auto &philosopher : philosophersThreads)
     {
         philosopher.join();
